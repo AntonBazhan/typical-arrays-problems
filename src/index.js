ea454e635c41be4ce48018736e6a04942a92e9ca
Anton <bazhan.anton@ukr.net>
2020-10-01 15:09:31 +0300

    fix:3 task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 exports.min = function min(array) {
-    // const result = Array.from(arguments);
+    if (!Array.isArray(array) || array <= 0) {
+        return 0;
+    }
     return Math.min(...array);
 };
 exports.max = function max(array) {
+    if (!Array.isArray(array) || array <= 0) {
+        return 0;
+    }
     return Math.max(...array);
 };
 
 exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array <= 0) {
+        return 0;
+    }
     return array.reduce((a, b) => a + b) / array.length;
 };
